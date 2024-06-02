dd7f1982503277b1e7e2dc5cfcb8bb36770d78d1
__𝐌𝐎𝐇𝐒𝐈𝐍-𝐀𝐋𝐈 <167536098+mohsin-ali08@users.noreply.github.com>
2024-06-03 01:02:26 +0500

    Add files via upload

---
+++ Day # 4/main.js
@@ -0,0 +1,45 @@
+// create a const object 
+
+
+
+
+const product = {
+    nameOfProduct : "Parker Jotter Standard CT Ball Pen",
+    color : "Black",
+    rating : 1.5,
+    offer : 5,
+    price : 230,
+
+};
+console.log(product);
+
+
+
+const profile = {
+    userName : "mohsin-ali",
+    isFollow : true,
+    numberOfPost: 195,
+    numberOfFollowers : 5069,
+    totalFollowing : 4,
+    bioInfo: "hello world",
+    email:"mohsinali08@gmail.com",
+
+};
+
+console.log(profile);
+
+
+
+
+const apple ={
+        productName: "iPhone 12 pro",
+        color: "Graphite",
+        storageCapacity: "512GB",
+        price: 1399, // in dollars
+        releaseDate: "September 2021",
+        cameraSpecs: "Triple 12MP Ultra Wide, Wide, and Telephoto cameras",
+        displaySpecs: "6.7-inch Super Retina XDR display",
+        processor: "A15 Bionic chip"
+};
+
+console.log(apple);
