d693262a5c13786cbb5e5c7f6b44cc8c6b6e073d
__𝐌𝐎𝐇𝐒𝐈𝐍-𝐀𝐋𝐈 <167536098+mohsin-ali08@users.noreply.github.com>
2024-06-12 22:07:06 +0500

    Add files via upload

---
+++ Day # 9/main.js
@@ -0,0 +1,36 @@
+const user = {
+    apple: {
+        iphone6s: {
+            firstName: "John",
+            lastName: "Doe",
+            age: 50,
+            eyeColor: "blue"
+
+        },
+        iphone7: {
+            firstName: "John",
+            lastName: "Doe",
+            age: 50,
+            eyeColor: "blue"
+
+        },
+        iphone7plus: {
+            firstName: "John",
+            lastName: "Doe",
+            age: 50,
+            eyeColor: "blue"
+
+        },
+    },
+    android: {
+        samsung: {
+        modelName: "samsung s23",
+         color: "titanium"
+        },
+    },
+
+};
+
+console.log( `${user.apple}`);
+
+
